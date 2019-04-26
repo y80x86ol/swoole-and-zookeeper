a0041b88df631a47435a5ed36ac2caf0704c6247
ken <364810520@qq.com>
2019-04-26 18:24:35 +0800

    完成基本功能：1、节点选举2、tasker的work分配3、基础进程管理

--- app/Core/SwooleProcessPoolServer.php
+++ app/Core/SwooleProcessPoolServer.php
@@ -8,8 +8,11 @@
 
 namespace App\Core;
 
+use App\Common\Log;
+
 /**
  * swoole的process进程池方式启动应用
+ *
  * @package App\Core
  */
 class SwooleProcessPoolServer
@@ -23,13 +26,22 @@ class SwooleProcessPoolServer
         $pool = new \Swoole\Process\Pool($workerNum);
 
         $pool->on("WorkerStart", function ($pool, $workerId) {
+            Log::info("======== 进程池启动成功 ========");
+
+            $httpServer = new HttpServer();
+            $httpProcess = $httpServer->run();
+
+            $pingServer = new PingServer();
+            $pingProcess = $pingServer->run();
+
             $zooService = new ZooServer();
             $zooService->initData();
             $zooService->run();
         });
 
         $pool->on("WorkerStop", function ($pool, $workerId) {
-            echo "Worker#{$workerId} is stopped\n";
+            Log::info("======== 有进程停止了工作 ========" . $workerId);
+
         });
 
         $pool->start();
